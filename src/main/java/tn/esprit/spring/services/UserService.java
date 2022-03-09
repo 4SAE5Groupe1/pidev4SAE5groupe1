a85f0205b59335fa8eaa6df5de7dc630b32706ba
Fakhri-ab <92473176+Fakhri-ab@users.noreply.github.com>
2022-03-09 15:02:29 +0100

    all branch 4

--- src/main/java/tn/esprit/spring/services/UserService.java
+++ src/main/java/tn/esprit/spring/services/UserService.java
@@ -2,7 +2,6 @@ package tn.esprit.spring.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tn.esprit.spring.DAO.UserDAO;
 import tn.esprit.spring.entites.User;
 import tn.esprit.spring.repositories.UserRepository;
 
